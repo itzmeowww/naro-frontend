0823119477af812369a065cd3ece3c446d413aad
itzmeowww <winthanasan@gmail.com>
2023-06-10 16:32:09 +0900

    update: add todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+interface Task {
+  name: string
+  status: boolean
+  id: number
+}
+
+const tasks = ref<Task[]>([])
+const newTaskName = ref('')
+
+const addTask = () => {
+  if (newTaskName.value !== '')
+    tasks.value.push({ name: newTaskName.value, status: false, id: tasks.value.length })
+  newTaskName.value = ''
+}
+
+const toggleTask = (idx: number) => {
+  tasks.value[idx].status = !tasks.value[idx].status
+}
+</script>
+
+<template>
+  <section class="">
+    <header>完了済み</header>
+    <ul>
+      <li
+        v-for="task in tasks.filter((x) => x.status)"
+        :key="task.name"
+        @click="toggleTask(task.id)"
+      >
+        {{ task.name }}
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <header>未完</header>
+    <ul>
+      <li
+        v-for="task in tasks.filter((x) => !x.status)"
+        :key="task.name"
+        @click="toggleTask(task.id)"
+      >
+        {{ task.name }}
+      </li>
+    </ul>
+    <p>タスクの名前をクリックすると、そのタスクが完了します。</p>
+  </section>
+
+  <div>
+    <label>
+      名前
+      <input v-model="newTaskName" type="text" />
+    </label>
+
+    <button @click="addTask">add</button>
+  </div>
+</template>
+
+<style scoped>
+section {
+}
+li:hover {
+  text-decoration: line-through;
+  cursor: pointer;
+}
+</style>
